7b5eba78d2c44c0a6e976cf04c95a60df28599b0
Serik <eskermesovv@gmail.com>
2023-10-25 01:50:36 +0600

    add routing and home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./modules/home/home.component";
+import {QuizComponent} from "./modules/quiz/quiz.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'quiz', component: QuizComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { QuizComponent } from './modules/quiz/quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
