a69ba26df1c9ac9f01913a0c6339834ca4ccb965
Serik <eskermesovv@gmail.com>
2024-03-21 21:49:56 +0500

    fix confetti

--- src/app/modules/congrats/congrats.component.ts
+++ src/app/modules/congrats/congrats.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import confetti from 'canvas-confetti';
 
 
@@ -8,76 +8,41 @@ import confetti from 'canvas-confetti';
   styleUrls: ['./congrats.component.scss']
 })
 export class CongratsComponent {
-  isClicked = false;
-  confettiTriggered = false;
-  isButtonDisabled = false;
-
-  randomInRange(min: number, max: number): number {
-    return Math.random() * (max - min) + min;
-  }
-
   onClick(): void {
     var count = 200;
     var defaults = {
-      origin: { y: 0.7 }
+      origin: {y: 0.7}
     };
 
-
-    if (this.isButtonDisabled) {
-      return;
+    function fire(particleRatio, opts) {
+      confetti({
+        ...defaults,
+        ...opts,
+        particleCount: Math.floor(count * particleRatio)
+      });
     }
 
-    if (this.isClicked && !this.confettiTriggered) {
-      this.isClicked = false;
-    } else if (!this.confettiTriggered) {
-      this.isClicked = true;
-
-      // confetti({
-      //   particleCount: 40,
-      //   spread: 100,
-      //   angle: this.randomInRange(55, 125),
-      //   colors: ['#fff367', '#001e4d', '#fff367']
-      // });
-
-      function fire(particleRatio, opts) {
-        confetti({
-          ...defaults,
-          ...opts,
-          particleCount: Math.floor(count * particleRatio)
-        });
-      }
-
-      fire(0.25, {
-        spread: 26,
-        startVelocity: 55,
-      });
-      fire(0.2, {
-        spread: 60,
-      });
-      fire(0.35, {
-        spread: 100,
-        decay: 0.91,
-        scalar: 0.8
-      });
-      fire(0.1, {
-        spread: 120,
-        startVelocity: 25,
-        decay: 0.92,
-        scalar: 1.2
-      });
-      fire(0.1, {
-        spread: 120,
-        startVelocity: 45,
-      });
-
-      this.confettiTriggered = true;
-
-      // this.isButtonDisabled = true;
-      setTimeout(() => {
-        this.isButtonDisabled = false;
-      }, 10);
-    } else {
-      this.confettiTriggered = false;
-    }
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+    fire(0.2, {
+      spread: 60,
+    });
+    fire(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
   }
 }
